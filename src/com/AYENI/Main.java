f7f208dbbb154ed0facddbb3a603f6ea0ef147e7
Bunmi86 <ay.bunmi@gmail.com>
2021-12-02 13:08:17 +0100

    My FirstJavaTask

---
+++ src/com/AYENI/Main.java
@@ -0,0 +1,12 @@
+package com.AYENI;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world, this is my new task");
+    System.out.print("This is my new task and it is going smoothly");
+
+    System.out.print("Have a Lovely day ahead");
+    }
+
+}
